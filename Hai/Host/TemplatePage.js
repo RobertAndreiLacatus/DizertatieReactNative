55be64d4cb2d027872a7891177bd3973ba2f549c
RobertAndreiLacatus <100790743+RobertAndreiLacatus@users.noreply.github.com>
2023-07-30 16:04:50 +0300

    Add files via upload

--- Hai/Host/TemplatePage.js
+++ Hai/Host/TemplatePage.js
@@ -1,10 +1,20 @@
-import { View, Text , StyleSheet, ScrollView, Image} from 'react-native'
+import { View, Text , StyleSheet, ScrollView, Image, Button,Alert,TouchableOpacity} from 'react-native'
 import React,{useState}from 'react'
 import { useRoute } from '@react-navigation/native';
 import {TextInput} from 'react-native'
 
 import { useNavigation } from '@react-navigation/native';
+import Bed from '../assets/bed.png'
+import Line from '../assets/thinline.png'
+import Bathroom from '../assets/bathroom.png'
+import People from '../assets/people.png'
+import Bedroom from '../assets/bedroom.png'
+import Kitchen from '../assets/kitchen.png'
+import LivingRoom from '../assets/livingroom.png'
+import Balcony from '../assets/balcony.png'
 
+import axios from 'axios';
+import { API_URL } from '../HttpService';
 
 
 
@@ -16,23 +26,195 @@ const TemplateH=() =>{
   const route=useRoute()
   const {title,description,image} = route.params 
   const [inboxText, setInboxText] = useState('');
+  const [inboxText1, setInboxText1] = useState('');
+  const [inboxText2, setInboxText2] = useState('');
+  const [detailsText,setDetailsText] =useState('');
+  const [detailsText1,setDetailsText1] =useState('');
+  const [detailsText2,setDetailsText2] =useState('');
+  const [detailsText3,setDetailsText3] =useState('');
+  const [descriptionB, setDescriptionB]=useState('');
+  const [place, setPlace] = useState('');
+  // const [region, setRegion] = useState({
+  //   latitude: 37.78825,  // Initial map center latitude
+  //   longitude: -122.4324,  // Initial map center longitude
+  //   latitudeDelta: 0.0922,  // Zoom level (latitude delta)
+  //   longitudeDelta: 0.0421,  // Zoom level (longitude delta)
+  // });
+    // Perform any necessary geocoding to get the latitude and longitude for the entered place
+  // const handleSearch = () => {
+  //   // You can use a geocoding API service like Google Maps Geocoding API or OpenCage Geocoding API
+
+  //   // Once you have the latitude and longitude, update the region state to refresh the map
+  //   setRegion({
+  //     latitude: 41.3874,
+  //     longitude: 2.1686,
+  //     latitudeDelta: 0.0922,
+  //     longitudeDelta: 0.0421,
+  //   });
+  // };
+
+  const handlePageInfo= ()=>{
+
+    var data = {
+      'title':title,
+      'description':description,
+      'image':image,
+      'inboxText':inboxText,
+      'inboxText1':inboxText1,
+      'inboxText2':inboxText2,
+      'detailsText':detailsText,
+      'detailsText1':detailsText1,
+      'detailsText2':detailsText2,
+      'detailsText3':detailsText3,
+      'descriptionB':descriptionB,
+      
+
+    };
+
+    const resetInputs = () => {
+      setInboxText('');
+      setInboxText1('');
+      setInboxText2('');
+      setDetailsText('');
+      setDetailsText1('');
+      setDetailsText2('');
+      setDetailsText3('');
+      setDescriptionB('');
+    };
+
+    if (data) {
+      axios
+          .post(API_URL + '/templateUser',data)
+          .then((response)=>{
+            Alert.alert(response.data.message);
+          resetInputs();
+          
+
+          })
+          .catch((error)=>{
+            console.error(error);
+          })
+    } else{
+      Alert.alert('Please fill in all the fields')
+    }
+
+    
+  }
+
+  
  
   return (
     
     <ScrollView >
         <View style={styles.container}>
-            <Text style={styles.title}>Template {title} </Text>
-            <Text style={styles.description}>Template {description} </Text>
+            <Text style={styles.title}> {title} </Text>
+            <Text style={styles.description}>{description} </Text>
             <Image source={{ uri: image }} style={styles.image} />
+            <Image source={Line} style={styles.line}/>
             <TextInput
               style={styles.inboxInput}
               value={inboxText}
               onChangeText={setInboxText}
-              placeholder="Enter inbox text"
+              placeholder="Enter the number of beds"
+              placeholderTextColor="#999"
+              multiline
+              textAlignVertical="top"
+      />    
+            <Image source={Bed} style={styles.imageIcon}/>
+            <TextInput
+              style={styles.inboxInput1}
+              value={inboxText1}
+              onChangeText={setInboxText1}
+              placeholder="Enter the number of bathrooms"
               placeholderTextColor="#999"
               multiline
               textAlignVertical="top"
       />
+             <Image source={Bathroom} style={styles.imageIcon1}/> 
+
+             <TextInput
+              style={styles.inboxInput2}
+              value={inboxText2}
+              onChangeText={setInboxText2}
+              placeholder="Enter the number of people"
+              placeholderTextColor="#999"
+              multiline
+              textAlignVertical="top"
+      />
+            <Image source={People} style={styles.imageIcon2}/> 
+            <Image source={Line} style={styles.line11}/>
+        <View style={styles.containerDetails}>
+          <Text style={styles.titleOther}>Other details</Text>
+          <TextInput
+              style={styles.detailsText}
+              value={detailsText}
+              onChangeText={setDetailsText}
+              placeholder="Describe your Bedroom"
+              placeholderTextColor="#999"
+              multiline
+              textAlignVertical="top"
+      />
+            <Image source={Bedroom} style={styles.bedroomIcon}/>
+            <TextInput
+              style={styles.detailsText1}
+              value={detailsText1}
+              onChangeText={setDetailsText1}
+              placeholder="Describe your Kitchen"
+              placeholderTextColor="#999"
+              multiline
+              textAlignVertical="top"
+      />
+            <Image source={Kitchen} style={styles.bedroomIcon}/>
+            <TextInput
+              style={styles.detailsText2}
+              value={detailsText2}
+              onChangeText={setDetailsText2}
+              placeholder="Describe your Living Room"
+              placeholderTextColor="#999"
+              multiline
+              textAlignVertical="top"
+      />
+            <Image source={LivingRoom} style={styles.bedroomIcon}/>
+            <TextInput
+              style={styles.detailsText2}
+              value={detailsText3}
+              onChangeText={setDetailsText3}
+              placeholder="Describe your Balcony"
+              placeholderTextColor="#999"
+              multiline
+              textAlignVertical="top"
+      />
+            <Image source={Balcony} style={styles.bedroomIcon}/>
+        </View>
+        
+        <View style={styles.containerGeneral}>
+          <Text style={styles.titleGeneral}> General Description</Text>
+          <View style={styles.containerDescription2}> 
+            <Text style={styles.descriptionGeneral}> Please, make a general description about the surroundings. </Text>
+          </View>
+          <TextInput
+              style={styles.descriptionB}
+              value={descriptionB}
+              onChangeText={setDescriptionB}
+              placeholder="Describe your area"
+              placeholderTextColor="#999"
+              multiline
+              textAlignVertical="top"
+      />
+          
+
+         
+        </View>
+        <Image source={Line} style={styles.line3}/>
+        
+        
+          <TouchableOpacity onPress={handlePageInfo} style={styles.doneBtn}>
+            <Text> Done </Text>
+          </TouchableOpacity>
+          <View style={styles.footer}>
+                  <Text style={styles.footerText}>@ Lacatus Robert</Text>
+       
+            </View>
         </View>
     </ScrollView>
    
@@ -47,7 +229,7 @@ const styles = StyleSheet.create({
     padding: 20,
     alignItems: 'center',
     justifyContent: 'flex-start',
-    height:2000,
+    height:5000,
     width:'100%'
 
   },
@@ -71,14 +253,242 @@ const styles = StyleSheet.create({
     marginTop:100
   },
   inboxInput: {
-    fontSize: 30,
+
+    fontSize: 10,
+    backgroundColor:'white',
+    borderRadius:10
+,    fontWeight: 'bold',
+    borderWidth: 1,
+    borderColor: '#999',
+    padding: 20,
+    width: '70%',
+    height: 50,
+    marginTop:20,
+    left:10
+  },
+  imageIcon:{
+    width:60,
+    height:60,
+    right:150,
+    bottom:50
+  },
+  line:{
+    width:350,
+    height:50
+  },
+  inboxInput1: {
+    fontSize: 10,
     fontWeight: 'bold',
     borderWidth: 1,
     borderColor: '#999',
     padding: 20,
-    width: '100%',
-    height: 200,
-    marginTop:20
+    width: '70%',
+    height: 50,
+    bottom:40,
+    left:10,
+    backgroundColor:'white',
+    borderRadius:10
+  },
+  imageIcon1:{
+    width:50,
+    height:50,
+    right:150,
+    bottom:90
+  },
+  inboxInput2: {
+    fontSize: 10,
+    fontWeight: 'bold',
+    borderWidth: 1,
+    borderColor: '#999',
+    padding: 20,
+    width: '70%',
+    height: 50,
+    bottom:70,
+    left:10,
+    backgroundColor:'white',
+    borderRadius:10
+  },
+  imageIcon2:{
+    width:50,
+    height:50,
+    right:150,
+    bottom:120
+  },
+  line2:{
+    width:350,
+    height:50,
+    bottom:200
+  },
+  containerDetails:{
+    flex:0.5,
+    marginBottom:10,
+    
+    width:350,
+    height:20,
+    bottom:10
+  },
+  titleOther:{
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  detailsText:{
+    fontSize: 10,
+    fontWeight: 'bold',
+    borderWidth: 1,
+    borderColor: '#999',
+    padding: 20,
+    width: '70%',
+    height: 80,
+    top:20,
+    left:50,
+    backgroundColor:'white',
+    borderRadius:10
+  },
+  bedroomIcon:{
+    width:50,
+    height:50,
+    right:10,
+    bottom:50
+  },
+  
+  detailsText1:{
+    fontSize: 10,
+    fontWeight: 'bold',
+    borderWidth: 1,
+    borderColor: '#999',
+    padding: 20,
+    width: '70%',
+    height: 80,
+    top:10,
+    left:50,
+    
+    backgroundColor:'white',
+    borderRadius:10
+  },
+  detailsText2:{
+    fontSize: 10,
+    fontWeight: 'bold',
+    borderWidth: 1,
+    borderColor: '#999',
+    padding: 20,
+    width: '70%',
+    height: 80,
+    top:10,
+    left:50,
+    
+    backgroundColor:'white',
+    borderRadius:10
+  },
+  line2:{
+    width:350,
+    height:50,
+    bottom:1470
+  },
+  containerGeneral:{
+    flex:0.5,
+    marginBottom:10,
+    
+    width:350,
+    height:20,
+    bottom:1200,
+    
+  },
+  titleGeneral:{
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  line3:{
+    width:350,
+    height:50,
+    bottom:3100
+  },
+  line11:{
+    width:350,
+    height:50,
+    top:5
+  },
+
+  containerDescription2:{
+    flex:0.1,
+    marginBottom:10,
+ 
+    width:320,
+    height:50,
+    bottom:50,
+    
+    left:40,
+    justifyContent:'flex-start',
+    alignItems:'flex-start'
+  },
+  descriptionGeneral:{
+    fontSize:20,
+    color:'white',
+    position:'absolute',
+    marginTop:100,
+    justifyContent:'flex-start',
+    alignItems:'center',
+    bottom:5,
+    
+    
+    
+  },
+  descriptionB:{
+    fontSize: 10,
+    fontWeight: 'bold',
+    borderWidth: 1,
+    borderColor: '#999',
+    padding: 20,
+    width: '90%',
+    height: 400,
+    bottom:50,
+    left:20,
+    
+    backgroundColor:'white',
+    borderRadius:10
+
+  },
+  detailsTextMap:{
+    fontSize: 10,
+    fontWeight: 'bold',
+    borderWidth: 1,
+    borderColor: '#999',
+    padding: 20,
+    width: 250,
+    height: 30,
+    top:10,
+    left:30,
+    bottom:20,
+    backgroundColor:'white',
+    borderRadius:10
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(52, 52, 52, 0.9)',
+    height: 80,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  footerText: {
+    fontSize: 16,
+    color: 'white',
+  },
+
+  doneBtn: {
+    width: 120,
+    backgroundColor: '#fb5b5a',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 30,
+    bottom: 2450,
   },
     
    
